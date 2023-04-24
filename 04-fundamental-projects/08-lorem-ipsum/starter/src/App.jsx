a4eaacc6c3442aaea4f994bf95b9c44c984c8d49
r2d2 <arturo.galvis@hotmail.com>
2023-04-24 10:50:51 -0500

    2

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(1);
-  return <h2>Lorem Ipsum</h2>;
+  return (
+    <section className="section-center">
+      <h4>Lorem Ipsum</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">parrafos:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          max="8"
+          steps="1"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+      </form>
+    </section>
+  );
 };
 export default App;

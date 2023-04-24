4728fe686f0b9263ae223b52deac9aca24dd08af
r2d2 <arturo.galvis@hotmail.com>
2023-04-24 11:24:01 -0500

    3 final

--- 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -4,11 +4,17 @@ import data from './data';
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(typeof count);
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
 
   return (
     <section className="section-center">
       <h4>Lorem Ipsum</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">parrafos:</label>
         <input
           type="number"
@@ -24,6 +30,11 @@ const App = () => {
           generar
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}{' '}
+      </article>
     </section>
   );
 };

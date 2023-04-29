51ecca29506ee1c7dadbf7502c25c7f2647e0740
r2d2 <arturo.galvis@hotmail.com>
2023-04-29 12:14:41 -0500

    6 localstorage

--- 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/starter/src/App.jsx
@@ -2,15 +2,33 @@ import { nanoid } from 'nanoid';
 import Form from './Form';
 import { useState } from 'react';
 import Items from './Items';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  //console.log(list);
+  if (list) {
+    list = JSON.parse(localStorage.getItem('list'));
+  } else {
+    list = [];
+  }
+  return list;
+};
+const setLocalStorage = (items) => {
+  localStorage.setItem('list', JSON.stringify(items));
+};
+const defaultList = JSON.parse(localStorage.getItem('list') || '[]');
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(getLocalStorage());
   const addItem = (itemName) => {
     const newItem = { name: itemName, completed: false, id: nanoid() };
-    setItems([...items, newItem]);
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    setLocalStorage(newItems);
   };
   const removeItem = (itemId) => {
     const newItems = items.filter((item) => item.id !== itemId);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
   return (
     <section className="section-center">
